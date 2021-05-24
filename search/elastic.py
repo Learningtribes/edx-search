8737be4b3b9163d44afef35e70ae4d9ab372f1d9
rythmE <poiuwertion@gmail.com>
2021-05-24 10:36:03 +0800

    Added course_hashtag_list in course search

--- search/elastic.py
+++ search/elastic.py
@@ -596,7 +596,6 @@ class ElasticSearchEngine(SearchEngine):
 
         try:
             log.info("search body: %s", body)
-            print(" ------ search body --------\n {}".format(body))
             es_response = self._es.search(
                 index=self.index_name,
                 body=body,

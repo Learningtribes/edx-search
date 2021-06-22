b3578f7726a7fd9fc4294ae14beffba130a947df
Barry Paneer <barrypaneer@BarrydeMacBook-Pro.local>
2021-06-22 17:50:13 +0800

    updated

--- search/views.py
+++ search/views.py
@@ -11,7 +11,13 @@ from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST
 
 from eventtracking import tracker as track
-from .api import QueryParseError, perform_search, course_discovery_search, course_discovery_filter_fields
+from .api import (
+    QueryParseError,
+    perform_search,
+    course_discovery_search,
+    course_discovery_filter_fields,
+    programs_discovery_search
+)
 from .initializer import SearchInitializer
 
 # log appears to be standard name used for logger
@@ -243,6 +249,106 @@ def course_discovery(request):
         }
         log.exception(
             'Search view exception when searching for %s for user %s: %r',
+            search_term,
+            request.user.id,
+            err
+        )
+
+    return HttpResponse(
+        json.dumps(results, cls=DjangoJSONEncoder),
+        content_type='application/json',
+        status=status_code
+    )
+
+
+@require_POST
+def program_discovery(request):
+    """
+    Search for programs from ElasticSearch
+
+    Args:
+        request (required) - django request object
+
+    Returns:
+        http json response with the following fields
+            "took" - how many seconds the operation took
+            "total" - how many results were found
+            "max_score" - maximum score from these resutls
+            "results" - json array of result documents
+
+            or
+
+            "error" - displayable information about an error that occured on the server
+
+    POST Params:
+        "search_string" (optional) - text with which to search for courses
+        "page_size" (optional)- how many results to return per page (defaults to 20, with maximum cutoff at 100)
+        "page_index" (optional) - for which page (zero-indexed) to include results (defaults to 0)
+    """
+    results = {
+        "error": _("Nothing to search")
+    }
+    status_code = 500
+
+    search_term = request.POST.get("search_string", None)
+
+    try:
+        size, from_, page = _process_pagination_values(request)
+        field_dictionary = _process_field_values(request)
+
+        # Analytics - log search request
+        track.emit(
+            'edx.course_discovery.search.initiated',
+            {
+                "search_term": search_term,
+                "page_size": size,
+                "page_number": page,
+            }
+        )
+
+        results = programs_discovery_search(
+            search_term=search_term,
+            size=size,
+            from_=from_,
+            field_dictionary=field_dictionary,
+            user=request.user,
+            include_course_filter=True
+        )
+
+        log.info('%s programs find.', results['total'])
+
+        # Analytics - log search results before sending to browser
+        track.emit(
+            'edx.course_discovery.search.results_displayed',
+            {
+                "search_term": search_term,
+                "page_size": size,
+                "page_number": page,
+                "results_count": results["total"],
+            }
+        )
+
+        status_code = 200
+
+    except ValueError as invalid_err:
+        results = {
+            "error": unicode(invalid_err)
+        }
+        log.debug(unicode(invalid_err))
+
+    except QueryParseError:
+        results = {
+            "error": _('Your query seems malformed. Check for unmatched quotes.')
+        }
+
+    # Allow for broad exceptions here - this is an entry point from external reference
+    except Exception as err:
+        raise Exception(err)
+        results = {
+            "error": _('An error occurred when searching for "{search_string}"').format(search_string=search_term)
+        }
+        log.exception(
+            'Search view exception when searching for %s for user %s: %r : %s',
             search_term,
             request.user.id,
             err

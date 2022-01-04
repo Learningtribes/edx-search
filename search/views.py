d9b50d764d72f4a71f0954027be763d90022bd48
Jiayi <18341624+xujiayi1256@users.noreply.github.com>
2022-01-04 16:57:27 +0800

    Update views.py

--- search/views.py
+++ search/views.py
@@ -235,7 +235,7 @@ def course_discovery(request):
             field_dictionary=field_dictionary,
             user=request.user,
             allow_enrollment_end_filter=True,
-            sort_type=request.POST.get('sort_type', '')
+            sort_type=request.POST.get('sort_type')
         )
         log.info('%s courses find', results['total'])
 
@@ -355,7 +355,7 @@ def program_discovery(request):
             field_dictionary=field_dictionary,
             # user=request.user,
             include_course_filter=True,
-            sort_type=request.POST.get('sort_type', '')
+            sort_type=request.POST.get('sort_type')
         )
 
         log.info('%s programs find.', results['total'])

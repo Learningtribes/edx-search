4cbf6dbdc4443ca15ee0ef4f24ce21c2fd239261
Aaron Zhao <rudy710@qq.com>
2022-08-03 09:52:42 +0800

    test fuzzy

--- search/elastic.py
+++ search/elastic.py
@@ -630,7 +630,7 @@ class ElasticSearchEngine(SearchEngine):
                     "match": {
                         field: {
                             "query": query_string.encode('utf-8').translate(None, RESERVED_CHARACTERS),
-                            "fuzziness": "AUTO" if field != "content.number" else 0,
+                            "fuzziness": 1 if field != "content.number" else 0,
                             "operator": "AND",
                             "analyzer": "standard"
                         }

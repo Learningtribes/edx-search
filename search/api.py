1146135d6e18c0f11c7632a9e7d2fa22d834315e
Barry Paneer <barrypaneer@BarrydeMacBook-Pro.local>
2022-09-19 10:33:41 +0800

    Added: filter for archived courses

--- search/api.py
+++ search/api.py
@@ -265,6 +265,15 @@ def course_discovery_search(search_term=None, size=20, from_=0, field_dictionary
     if getattr(settings, 'ALLOW_CATALOG_VISIBILITY_FILTER', False):
         use_field_dictionary['catalog_visibility'] = CATALOG_VISIBILITY_CATALOG_AND_ABOUT
 
+    exclude = search_fields.get('exclude', None)
+    if 'archived' == exclude:
+        filter_dictionary.update(
+            {
+                'start': _format_filter(DateRange(None, datetime.utcnow())),
+                'end': _format_filter(DateRange(datetime.utcnow(), None))
+            }
+        )
+
     results = searcher.search(
         query_string=search_term,
         doc_type="course_info",

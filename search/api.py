326dbbb5d73328d5053d126474e6851163a69166
Barry Paneer <barrypaneer@Barry-MacBook-Pro-4-Working.local>
2023-06-01 14:03:52 +0800

    Fixed

--- search/api.py
+++ search/api.py
@@ -360,7 +360,6 @@ def programs_discovery_search(search_term=None, size=20, from_=0, field_dictiona
     if 'archived' == exclude:
         filter_dictionary.update(
             {
-                'start': _format_filter(DateRange(None, datetime.utcnow())),
                 'end': _format_filter(DateRange(datetime.utcnow(), None))
             }
         )

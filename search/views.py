9db5ffbfdf6d75cbe87ba10a89c892df6f80edd5
Noah Wang <234082230@qq.com>
2024-04-19 18:34:21 +0800

    Fix for course tags or categories with , (#33)

--- search/views.py
+++ search/views.py
@@ -60,7 +60,7 @@ def _process_field_values(request, allowed_fields):
         elif field_key in allowed_fields:
             filter_values = request.POST[field_key]
 
-            if ',' in filter_values:
+            if field_key not in ['vendor', 'course_category'] and ',' in filter_values:
                 field_values[field_key] = filter_values.split(',')
                 continue
 

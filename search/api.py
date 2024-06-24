54cce8100e8a66d23f823efc1ca788e3250e9916
Barry Bao <barrybao@Barrys-MacBook-Pro-2.local>
2024-06-24 18:19:54 +0800

    supported: for course code

--- search/api.py
+++ search/api.py
@@ -241,6 +241,10 @@ def course_discovery_search(search_term=None, size=20, from_=0, field_dictionary
             sort_args = [{'raw_display_name': {'order': 'asc'}}, {'start': {'order': 'desc'}}]
         elif sort_args == '-display_name':
             sort_args = [{'raw_display_name': {'order': 'desc'}}, {'start': {'order': 'desc'}}]
+        elif sort_args == '+course':
+            sort_args = [{'course': {'order': 'asc'}}, {'start': {'order': 'desc'}}]
+        elif sort_args == '-course':
+            sort_args = [{'course': {'order': 'desc'}}, {'start': {'order': 'desc'}}]
         elif sort_args == '+created':
             sort_args = [{'created': {'order': 'asc'}}, {'start': {'order': 'desc'}}]
         elif sort_args == '-created':
